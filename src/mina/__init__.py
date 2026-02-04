b2d74f26319fa338fe27432f643dff61d8ce1f25
Ricardo O. Ramirez Flores <flores@ebi.ac.uk>
2026-02-04 17:17:10 +0000

    fixing package naming

--- src/mina/__init__.py
+++ src/mina/__init__.py
@@ -1,7 +1,12 @@
-from importlib.metadata import version
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version
 
 from . import down, pl, up
 
 __all__ = ["pl", "up", "down"]
 
-__version__ = version("MINA")
+try:
+    __version__ = version(__name__)  # resolves to "mina"
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0+unknown"
